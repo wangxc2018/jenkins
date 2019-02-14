ab19f27b9ef67a5691652765820ee6cf01edb641
wangxc2018 <1035371385@qq.com>
2019-02-14 15:59:45 +0800

    first commit

---
+++ src/main/java/com/lenovo/jenkins/JenkinsApplication.java
@@ -0,0 +1,14 @@
+package com.lenovo.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsApplication.class, args);
+	}
+
+}
+
